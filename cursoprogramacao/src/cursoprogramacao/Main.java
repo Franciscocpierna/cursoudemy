09588794ccdae198c1d5f959d1510c5672909dba
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2024-06-16 08:33:53 -0300

    exec 16/06/2024

--- cursoprogramacao/src/cursoprogramacao/Main.java
+++ cursoprogramacao/src/cursoprogramacao/Main.java
@@ -1,10 +1,32 @@
 package cursoprogramacao;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Ola mundo");
+		Locale.setDefault(Locale.US);
+		String n;
+		Scanner sc = new Scanner(System.in);
+		/*String x;
+		x = sc.next();
+        System.out.println("voce digitou o valor "+x);
+        sc.close();
+          */
+		/*int x;
+		x= sc.nextInt();
+		System.out.println("voce digitou o valor "+x);
+		sc.close();*/
+	
+		double x;
+		System.out.print("Entre com o numero:");
+		x= sc.nextDouble();
+		n= sc.next();
+		System.out.printf("voce digitou o valor: %.2f%n ",x);
+		System.out.println("vc digitou:"+n);
+		sc.close();
 	}
 
 }
---
+++ quebradelinha/src/quebradelinha/Quebradelinha.java
@@ -0,0 +1,21 @@
+package quebradelinha;
+
+import java.util.Scanner;
+
+public class Quebradelinha {
+
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1,s2,s3;
+		Scanner xl = new Scanner(System.in);
+		s1=xl.nextLine();
+		s2=xl.nextLine();
+		s3=xl.nextLine();
+        xl.close(); 
+        System.out.println("Dados digitados: ");
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+	}
+
+}
