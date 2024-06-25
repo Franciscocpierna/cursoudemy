24e82ce4a592b90d023d03e761a4714160d2d6df
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2024-06-25 09:32:48 -0300

    exer poo 25/06/2024

--- Coursesemppo/src/cursocompoo/Triangulo.java
+++ Coursesemppo/src/cursocompoo/Triangulo.java
@@ -1,9 +1,19 @@
 package cursocompoo;
 
 public class Triangulo {
-	public Double a;
+	public double a;
 	public double b;
 	public double c;
+	
+    public double area(){
+		double p=(a+b+c)/2;
+		return Math.sqrt(p*(p-a)*(p-b)*(p-c));
+		
+		
+	} 
+  
+	
+
 	
 
 }
--- Coursesemppo/src/cursosemopoo/Course.java
+++ Coursesemppo/src/cursosemopoo/Course.java
@@ -12,7 +12,7 @@ public class Course {
 		Locale.setDefault(Locale.US);
 		//double xA,xB,xC,yA,yB,yC;
 		//Triangulo x, y;  // declara assim tipo Triangulo
-		Triangulo x= new Triangulo(); // essa forma declara junto o tipo
+		Triangulo x= new Triangulo(); // ou diretamenyte
 		Triangulo y= new Triangulo(); 
         Scanner sc= new Scanner(System.in);
         System.out.print("Entre com  os tres lados do Trianglo X:");
@@ -23,9 +23,9 @@ public class Course {
         x.b = sc.nextDouble();
         x.c = sc.nextDouble();
        
-        double p = (x.a+x.b+x.c)/2; 
-        double areaX = Math.sqrt(p*(p-x.a)*(p-x.b)*(p-x.c));
-       
+        //double p = (x.a+x.b+x.c)/2; 
+        //double areaX = Math.sqrt(p*(p-x.a)*(p-x.b)*(p-x.c));
+        double areaX = x.area(); 
         System.out.printf("Area do triangulo X é: %.4f%n ", areaX);
         System.out.print("Entre com  os tres lados do Trianglo Y:");
         //yA= sc.nextDouble();
@@ -34,8 +34,9 @@ public class Course {
         y.a= sc.nextDouble();
         y.b= sc.nextDouble(); //com poo
         y.c = sc.nextDouble();
-        p = (y.a+y.b+y.c)/2;  
-        double areaY = Math.sqrt(p*(p-y.a)*(p-y.b)*(p-y.c));
+        //p = (y.a+y.b+y.c)/2;  
+        //double areaY = Math.sqrt(p*(p-y.a)*(p-y.b)*(p-y.c));
+        double areaY = y.area(); 
         System.out.printf("Area do triangulo Y é: %.4f%n ", areaY);
         if (areaX > areaY) {
          System.out.println("A area X é maior que Y");
