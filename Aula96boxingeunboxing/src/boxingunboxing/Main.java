c869e52775ef2e64195b6972b610f540b5eca245
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2024-07-25 09:16:25 -0300

    exec 25/07/2024

--- Aula96boxingeunboxing/src/boxingunboxing/Main.java
+++ Aula96boxingeunboxing/src/boxingunboxing/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		int x = 20; // aloca uma caixa na menoria stack uma caixa com 20
 		//Object obj = x; //objeto aponta para uma referencia a 20 na na memopria Heap
 		Integer obj = x; // Integer é objeto já compativelpai do int não precisa do cast
+		//Integer é wapeper classes
 		System.out.println(obj); 
         //int y = (int )obj; nesse caso usando Object preciso usar cast (int) para compatibilizar com tipo int
 		int y=obj*2;
