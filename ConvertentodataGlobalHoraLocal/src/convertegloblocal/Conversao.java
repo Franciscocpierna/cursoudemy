0e8292103724537a88c65141188989a95f027898
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2024-08-01 15:21:51 -0300

    exec 01/08/2024

---
+++ ConvertentodataGlobalHoraLocal/src/convertegloblocal/Conversao.java
@@ -0,0 +1,33 @@
+package convertegloblocal;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class Conversao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LocalDate d04 = LocalDate.parse("2024-07-31");
+		LocalDateTime d05 = LocalDateTime.parse("2024-07-31T01:30:26");
+		Instant d06 = Instant.parse("2024-07-31T01:30:26Z");
+		/*for(String s: ZoneId.getAvailableZoneIds()) {
+			System.out.println(s);
+		}*/
+		LocalDate r1 = LocalDate.ofInstant(d06,ZoneId.systemDefault());
+		System.out.println("r1 = "+r1);
+		LocalDate r2 = LocalDate.ofInstant(d06,ZoneId.of("Portugal"));
+		System.out.println("r2 = "+r2);
+		LocalDateTime r3 = LocalDateTime.ofInstant(d06,ZoneId.systemDefault());
+		LocalDateTime r4 = LocalDateTime.ofInstant(d06,ZoneId.of("Portugal"));
+		System.out.println("r3 = "+r3);
+		System.out.println("r4 = "+r4);
+		System.out.println("d04 dia= "+d04.getDayOfMonth());
+		System.out.println("d04 mes = "+d04.getMonthValue());
+		System.out.println("d05 hora = "+d05.getHour());
+		System.out.println("d05 hora = "+d05.getMinute());
+	}
+
+}
--- DatahoraOperacao/src/datahora/Data.java
+++ DatahoraOperacao/src/datahora/Data.java
@@ -3,6 +3,7 @@ package datahora;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class Data {
@@ -10,7 +11,7 @@ public class Data {
 	public static void main(String[] args) {
 		LocalDate d01 =LocalDate.now();
 		LocalDateTime d02 =LocalDateTime.now();
-		Instant d03=Instant.now();
+	/*	Instant d03=Instant.now();
 		LocalDate d04 = LocalDate.parse("2024-07-31");
 		LocalDateTime d05 = LocalDateTime.parse("2024-07-31T01:30:26");
 		Instant d06 = Instant.parse("2024-07-31T01:30:26Z");
@@ -21,8 +22,8 @@ public class Data {
 		LocalDateTime d09 = LocalDateTime.parse("31/07/2024 01:30",fmt2);
 		LocalDate d10 = LocalDate.of(2024, 7, 31);
 		LocalDateTime d11 = LocalDateTime.of(2024, 7, 31, 01, 30);
-		
-		System.out.println("d01 = "+d01.toString());//implicitamente nos outros poderia usar toString() dando mesmo resultado
+		*/
+		/*System.out.println("d01 = "+d01.toString());//implicitamente nos outros poderia usar toString() dando mesmo resultado
 		System.out.println("d02 = "+d02);
 		System.out.println("d03 = "+d03);
 		System.out.println("d05 = "+d05);
@@ -32,5 +33,24 @@ public class Data {
 		System.out.println("d09 = "+d09.toString());
 		System.out.println("d10 = "+d10.toString());
 		System.out.println("d11 = "+d11.toString());
+		*/
+		DateTimeFormatter fmt1=DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		LocalDate d04 = LocalDate.parse("2024-07-31");
+		System.out.println("d04 = "+d04.format(fmt1));// ou
+		System.out.println("d04 = "+fmt1.format(d04)); //ou
+		System.out.println("d04 = "+d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"); 
+		LocalDateTime d09 = LocalDateTime.parse("31/07/2024 01:30",fmt2);
+		System.out.println("d09 = "+fmt2.format(d09)); 
+		DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+		Instant d06 = Instant.parse("2024-07-31T01:30:26Z");
+		System.out.println("d06 = "+fmt3.format(d06)); 
+		DateTimeFormatter fmt5= DateTimeFormatter.ISO_INSTANT;
+		DateTimeFormatter fmt4= DateTimeFormatter.ISO_DATE_TIME;
+		LocalDateTime d05 = LocalDateTime.parse("2024-07-31T01:30:26");
+		System.out.println("d05 = "+fmt4.format(d05)); //OU
+		System.out.println("d05 = "+d05.format(fmt4));// 
+		System.out.println("d06 = "+fmt5.format(d06));//ou
+		System.out.println("d06 = "+d06.toString());
 	}    
 }
