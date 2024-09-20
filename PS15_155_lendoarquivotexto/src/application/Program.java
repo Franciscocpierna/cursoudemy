8058ef2684d9b03feaf88bb0d648851ee787bcc1
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2024-09-20 09:46:06 -0300

    projetos 20/09/2024

--- PS15_155_lendoarquivotexto/src/application/Program.java
+++ PS15_155_lendoarquivotexto/src/application/Program.java
@@ -4,21 +4,21 @@ import java.io.IOException;
 import java.util.Scanner;
 public class Program {
 	public static void main(String[] args) {
-	File file = new File("C:\\temp\\in.txt");
-	Scanner sc = null;
-	try {
-	sc = new Scanner(file);
-	while (sc.hasNextLine()) {
-	System.out.println(sc.nextLine());
-	}
+		File file = new File("C:\\java_projeto\\deuspatriafamila.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+		}
 	}
 	catch (IOException e) {
-	System.out.println("Error: " + e.getMessage());
+	    System.out.println("Error: " + e.getMessage());
 	}
 	finally {
-	if (sc != null) {
-	sc.close();
-}
+		if (sc != null) {
+		sc.close();
+	}
 }
 }
 }
